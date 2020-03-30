5cbe89614567799c048607f9ff64355a7773fcdd
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-03-30 12:56:26 -0500

    Implemented get, delete, put

--- backend/src/main/java/com/osai/backend/TaskController.java
+++ backend/src/main/java/com/osai/backend/TaskController.java
@@ -4,8 +4,10 @@ package com.osai.backend;
 
 // import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +21,7 @@ class TaskController {
         this.repository = repository;
     }
 
-//Mappings
+    //Mappings
     @PostMapping(path="/createTask")
     public Task createTask(@RequestBody Task newTask){
         return repository.save(newTask);
@@ -27,8 +29,29 @@ class TaskController {
 
     //Get all -- mostly for testing
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<Task> getTaskById(){
+    public @ResponseBody Iterable<Task> getAllTasks(){
         return repository.findAll();
     }
+    
+    @GetMapping("/getTaskById/{id}")
+    public Task getTaskById(@PathVariable long id) {
+         return repository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
+    }
 
+    @DeleteMapping("/deleteTaskById/{id}") 
+    public void deleteTaskById(@PathVariable long id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/updateTaskById/{id}")
+    public Task updateTaskById(@RequestBody Task newTask, @PathVariable long id) {
+        return repository.findById(id)
+            .map(task -> {
+                repository.deleteById(id);
+                return repository.save(newTask);
+            }).orElseGet(() -> {
+                return repository.save(newTask);
+            });
+    }
+    
 }
