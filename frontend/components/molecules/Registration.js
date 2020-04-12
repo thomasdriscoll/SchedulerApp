b1adec0416dffd603adb20116071e3860f724b3d
gprakarsh <379@doonschool.com>
2020-04-13 02:27:11 +0530

    added custom button1 and configure registration screen

--- frontend/components/molecules/Registration.js
+++ frontend/components/molecules/Registration.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, StyleSheet} from 'react-native';
+import Button1 from '../atoms/Button1';
 
-export default function CreateTask(){
+export default function Registration(){
     return(
-        <View>
-            <Text>Registration</Text>
+        <View style={styles.container}>
+            <View style={styles.subCon1}>
+                <Text style={styles.header}>Welcome to Schedelta</Text>
+                <Text style={styles.text}>A new way of scheduling</Text>
+                <Button1 text={`Let's go!`} onPress={()=>console.log('button hit')}/>
+            </View>
+            <View style={styles.subCon2}>
+                <Text style={styles.signInRedirect}>Already have a Schedelta account? Sign in</Text>
+            </View>
         </View>
     )
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'black',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    subCon2:{
+        position:"absolute",
+        bottom:60
+    },
+    header:{
+        color:'white',
+        fontSize:25,
+        textAlign:'center',
+    },
+    text:{
+        color:'#cfcfcf',
+        fontSize:18,
+        textAlign:'center',
+        marginTop:20,
+        marginBottom:20,
+    },
+    signInRedirect:{
+        fontSize:14,
+        color:'#bdbdbd'
+    }
+})
