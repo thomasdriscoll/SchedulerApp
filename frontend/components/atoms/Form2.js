aed9d356b3d63bc7f6bafe1af217876621541553
gprakarsh <379@doonschool.com>
2020-04-16 02:46:00 +0530

    Registration forms ready to integrate with backend

---
+++ frontend/components/atoms/Form2.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput,Keyboard, KeyboardAvoidingView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Form2({setForm,form}) {
+    // Keyboard.show()
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+            <Text style={styles.text}>What's your email?</Text>
+            <TextInput style={styles.input} keyboardType='email-address' autoCapitalize="none" placeholder="Your email" selectionColor="white" placeholderTextColor="rgba(255,255,255,0.5)"/>
+            <Text style={styles.nextIcon} onPress={()=>setForm(form+1)}><MaterialIcons size="40" color="white" name="navigate-next"/></Text>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal:'10%'
+    },
+    text:{
+        fontSize:28,
+        color:'white',
+        fontWeight:'bold',
+        width:'100%',
+        // backgroundColor:'red'
+    },
+    input: {
+        marginTop:20,
+        height:40,
+        width:'100%',
+        height:45,
+        borderBottomColor:'rgba(100,100,100,0.8)',
+        borderBottomWidth:0.3,
+        fontSize:28,
+        color:'white',
+        paddingRight:'13%'        
+    },
+    nextIcon:{
+        backgroundColor:'rgba(100,100,100,0.5)',
+        position:"relative",
+        bottom:45,
+        left:'44%'
+    }
+})
--- frontend/components/molecules/Registration.js
+++ frontend/components/molecules/Registration.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View,Text, StyleSheet} from 'react-native';
 import Button1 from '../atoms/Button1';
+import Form1 from '../atoms/Form1';
+import Form2 from '../atoms/Form2';
+import Form3 from '../atoms/Form3';
 
 export default function Registration({history}){
-    return(
+    const [form,setForm] = useState(0);
+    // setForm(0);
+    // Keyboard.show()
+
+    console.log(form);
+    if(form==1){
+        return <Form1 form={form} setForm={setForm}/>
+    }
+    else if(form==2){
+        return <Form2 form={form} setForm={setForm}/>
+    }
+    else if(form==3){
+        return <Form3 form={form} setForm={setForm}/>
+    }
+    return( 
         <View style={styles.container}>
             <View style={styles.subCon1}>
                 <Text style={styles.header}>Welcome to Schedelta</Text>
                 <Text style={styles.text}>A new way of scheduling</Text>
-                <Button1 text={`Let's go!`} onPress={()=>console.log('button hit')}/>
+                <Button1 text={`Let's go!`} onPress={()=>setForm(form+1)}/>
             </View>
             <View style={styles.subCon2}>
                 <Text style={styles.signInRedirect} onPress={()=>console.log('sign redirect hit')}>Already have a Schedelta account? Sign in</Text>
